e1e37291a73ec7568fb60a58892f5a851b325e09
Corentin-PC <corentin.lubeigt@tesa.prd.fr>
2023-04-14 14:54:10 +0200

    removed useless variables

--- src/single_source_MLE.m
+++ src/single_source_MLE.m
@@ -1,4 +1,4 @@
-function [i_tau_est, Fd_est, rho_est, phi_est,sigma2_est] = single_source_MLE(data, signal_clean, data_doppler, Fd_axis, OSF)
+function [i_tau_est, Fd_est, rho_est, phi_est,sigma2_est] = single_source_MLE(data, replica, data_doppler, Fd_axis, OSF)
 %--------------------------------------------------------------------------
 %        AUTHOR: Corentin Lubeigt 
 %                Lorenzo Ortega
@@ -6,9 +6,9 @@ function [i_tau_est, Fd_est, rho_est, phi_est,sigma2_est] = single_source_MLE(da
 %   
 %   DESCRIPTION: 
 %
-%        INPUTS: data      []      Fourier transform of the noisy composed 
+%        INPUTS: data         []   Fourier transform of the noisy composed 
 %                                  signal whose parameters we try to estimate
-%                signal_clean []   clean version of the main signal (no delay,
+%                replica      []   clean version of the main signal (no delay,
 %                                  no phase, no doppler) oversampled
 %                data_doppler []
 %                Fd_axis      [Hz]
@@ -28,27 +28,21 @@ function [i_tau_est, Fd_est, rho_est, phi_est,sigma2_est] = single_source_MLE(da
 %--------------------------------------------------------------------------
 
 %% I - Initialisation
-i_tau_range = floor(length(signal_clean)/2);
-% if OSF==1
-dataSamp = signal_clean;
-% else
-%     [dataSamp, ~] = adapt_signal(signal_clean,OSF);
-% end
+
+i_tau_range = floor(length(replica)/2);
 
 % Fourier Transforms of the data_clean set for each possible doppler in
 % b_axis
 FT_data = fft(data);
-FT_data_doppler_clean = fft(dataSamp.*data_doppler);
+FT_data_doppler_clean = fft(replica.*data_doppler);
 FTc_data_doppler_clean = conj(FT_data_doppler_clean);
 
-normCorr	= sqrt(sum(abs(dataSamp).^2));
+normCorr	= sqrt(sum(abs(replica).^2));
    
 %% II - Loop
 
-FT_data_current = FT_data;
-
 % a- Estimation of the main signal parameters
-corrFunc_data_current = fftshift(ifft(FT_data_current.*FTc_data_doppler_clean,[],1),1)./(abs(normCorr^2));
+corrFunc_data_current = fftshift(ifft(FT_data.*FTc_data_doppler_clean,[],1),1)./(abs(normCorr^2));
 
 % a1) delay/doppler (tau/Fd)
 [max_main,i_tau_max] = max(abs(corrFunc_data_current).^2,[],2);
@@ -80,7 +74,7 @@ end
 i_tau_est = i_tau_est - i_tau_range*OSF - 1;
 
 % b- estimate sigma2
-% sigma2_est = var(data - alpha_est*exp(1j*phi_est)*circshift(dataSamp.*(data_doppler(:,i_Fd_est)),i_tau_est));
+% sigma2_est = var(data - alpha_est*exp(1j*phi_est)*circshift(replica.*(data_doppler(:,i_Fd_est)),i_tau_est));
 sigma2_est = var(data);
 
 end % end of single_source_MLE function
